3512bf0aeab36ddc1310e6daf3d89975a85ccc2c
Israel Nazareth <israel.fab@hotmail.com>
2023-01-12 19:13:18 -0300

    style: create History components

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,34 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Array(7).fill(
+              <tr>
+                <td>Alguma tarefa</td>
+                <td>20 minutos</td>
+                <td>Há 2 meses</td>
+                <td>
+                  <Status statusColor="green">Concluído</Status>
+                </td>
+              </tr>,
+            )}
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
